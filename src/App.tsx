c836c2138daeaa921327857e6a8a294a732dfc29
Kelton <kelton.tinho23@gmail.com>
2024-06-27 14:56:21 -0300

    Adicionando sobre

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-    <h1 className='text-sky-300 text-6xl'>ola</h1>
+    
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import Sobre from './pages/sobre/Sobre.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Sobre />
   </React.StrictMode>,
 )
