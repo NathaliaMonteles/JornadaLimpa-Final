350a68d5153854e901c998f89c0db43cceab4d72
James Moura <jheyharrison@gmail.com>
2024-07-09 19:53:46 -0300

    correções

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../context/AuthContext'
-
-
+import { HouseLine} from '@phosphor-icons/react'
+import "./Navbar.css"
 
 function Navbar() {
   let navigate = useNavigate()
@@ -22,14 +22,14 @@ function Navbar() {
     <>
      <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
-          <Link to='/' className='text-2xl font-bold uppercase'>Jornada Limpa</Link>
+          <Link to='/' className='text-2xl font-bold uppercase flex gap-3'><HouseLine size={32} />Jornada Limpa</Link>
 
             <div className='flex gap-4'>
           
-              <Link to='/cadastro' className='hover:underline'>Cadastro</Link>
-              <Link to='/login' className='hover:underline'>Login</Link>
-              <Link to='/sobre' className='hover:underline'>Sobre</Link>
-              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+              <Link to='/cadastro' className='nav-item'>Cadastro</Link>
+              <Link to='/login' className='nav-item'>Login</Link>
+              <Link to='/sobre' className='nav-item'>Sobre</Link>
+              <Link to='' onClick={logout} className='nav-item'>Sair</Link>
              
             </div>
           </div>
