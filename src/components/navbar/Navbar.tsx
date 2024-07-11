cebf934e2c626ab6dfe000813c642bdb13445254
James Moura <jheyharrison@gmail.com>
2024-07-11 09:27:13 -0300

    manutenção navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,20 +1,22 @@
 import React, { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
-import { BsPencilSquare } from 'react-icons/bs'; 
-import { IoInformationCircleOutline } from 'react-icons/io5'; 
-import { MdExitToApp } from 'react-icons/md'; 
-import { HouseLine, UserCirclePlus } from '@phosphor-icons/react'; 
+import { BsPencilSquare } from 'react-icons/bs';
+import { IoInformationCircleOutline } from 'react-icons/io5';
+import { MdExitToApp } from 'react-icons/md';
+import { HouseLine, UserCirclePlus, CaretDown, UserCircleCheck } from '@phosphor-icons/react';
 import "./Navbar.css";
+
 
 function Navbar() {
   let navigate = useNavigate();
   const { usuario, handleLogout } = useContext(AuthContext);
   const [showSubMenu, setShowSubMenu] = useState(false);
 
+
   function logout() {
     handleLogout();
-    alert('UsuÃ¡rio deslogado com sucesso');
+    alert('Usuario deslogado com sucesso');
     navigate('/login');
   }
 
@@ -31,48 +33,64 @@ function Navbar() {
             <Link to='/categorias' className="flex items-center">
               Categoria
             </Link>
-            <div 
+            <div
               className='nav-item relative'
               onMouseEnter={() => setShowSubMenu(true)}
-              onMouseLeave={() => setShowSubMenu(false)}
-            >
-              <span><UserCirclePlus size={40} /></span>
-              {showSubMenu && (
-                <div className="dropdown-list">
-                  <ul>
-                    {!usuario.token && (
-                      <>
-                        <li>
-                          <Link to='/cadastro' className="flex items-center">
-                            <BsPencilSquare size={16} className="mr-1" /> Cadastro
-                          </Link>
-                        </li>
+              onMouseLeave={() => setShowSubMenu(false)}>
+
+
+              {!usuario.token && (
+                <>
+                  <span><UserCirclePlus size={40} /><CaretDown size={16} weight="fill" /></span>
+                  {showSubMenu && (
+                    <div className="dropdown-list">
+                      <ul>
+
                         <li>
                           <Link to='/login' className="flex items-center">
                             <BsPencilSquare size={16} className="mr-1" /> Login
                           </Link>
                         </li>
-                      </>
-                    )}
-                    {usuario.token && (
-                      <>
                         <li>
-                          <Link to='/sobre' className="flex items-center">
-                            <IoInformationCircleOutline size={16} className="mr-1" /> Sobre
+                          <Link to='/cadastro' className="flex items-center">
+                            <BsPencilSquare size={16} className="mr-1" /> Cadastro
                           </Link>
                         </li>
+
+                      </ul>
+                    </div>
+                  )}
+                </>
+              )}
+              {usuario.token && (
+                <>
+                  <span><UserCircleCheck size={40} weight="fill" /> <b>Olá, {usuario.nome}</b> </span>
+                  {showSubMenu && (
+                    <div className="dropdown-list">
+                      <ul>
+
                         <li>
                           <span onClick={logout} className="flex items-center cursor-pointer">
                             <MdExitToApp size={16} className="mr-1" /> Sair
                           </span>
                         </li>
-                      </>
-                    )}
-                  </ul>
-                </div>
+                        <li>
+                          <Link to='/sobre' className="flex items-center">
+                            <IoInformationCircleOutline size={16} className="mr-1" /> Sobre
+                          </Link>
+                        </li>
+
+                      </ul>
+                    </div>
+                  )}
+                </>
               )}
+
             </div>
           </div>
+
+
+
         </div>
       </div>
     </div>
